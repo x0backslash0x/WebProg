3008efa4d73fa09aaa91ac90d733e96727ab8905
x0backslash0x <paddy0524@gmail.com>
2024-10-02 18:55:08 +0200

    05 voeg opgave toe

--- labo02/pokemon.ts
+++ labo02/pokemon.ts
@@ -7,6 +7,17 @@
    labo 2 oefening 6
 
    ### OPGAVE ###
-   Gegeven is de volgende array van 20 pokemon:
+   * Gegeven is de volgende array van 20 pokemon:
+   * Maak een variabele team van het type string[].
+     Deze array bevat de pokemon van de gebruiker van het programma.
+   * Gebruik een lus om de pokemon te tonen aan de gebruiker.
+     Toon eerst de index gevolgd door de naam van de pokemon.
+     Je gebruikt dus nog NIET de ingebouwde keyInSelect van de readline-sync library. 
+   * Vraag daarna aan de gebruiker welke pokemon er moet toegevoegd worden aan het team.
+     Dit doe je aan de hand van de index van de pokemon. Dit doe je tot de gebruiker STOP ingeeft.
+     Je kan dit doen aan de hand van een do-while loop.
+   * Als de gebruiker een pokemon ingeeft die al in het team zet dan krijgt hij hiervan een melding en wordt de pokemon niet toegevoegd
+   * Als de pokemon niet bekend is (dus het ingegeven nummer groter is dan de lengte van de array) wordt er ook een melding gegeven
+   * Als je STOP hebt ingegeven dan wordt het team van de gebruiker op het scherm getoond
 
 */
