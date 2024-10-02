e4f8c92f407af8db47ea55b812ebdc2fb9ef5c5b
x0backslash0x <paddy0524@gmail.com>
2024-10-02 19:16:33 +0200

    oef 6 opgave bijwerken

--- labo02/pokemon.ts
+++ labo02/pokemon.ts
@@ -12,10 +12,12 @@
      Deze array bevat de pokemon van de gebruiker van het programma.
    * Gebruik een lus om de pokemon te tonen aan de gebruiker.
      Toon eerst de index gevolgd door de naam van de pokemon.
-     Je gebruikt dus nog NIET de ingebouwde keyInSelect van de readline-sync library. 
+     Je gebruikt dus nog NIET de ingebouwde keyInSelect van de readline-sync library.
+     --0. Bulbasaur
    * Vraag daarna aan de gebruiker welke pokemon er moet toegevoegd worden aan het team.
      Dit doe je aan de hand van de index van de pokemon. Dit doe je tot de gebruiker STOP ingeeft.
      Je kan dit doen aan de hand van een do-while loop.
+     --Welke pokemon wil je in je team? [0-20]: 4
    * Als de gebruiker een pokemon ingeeft die al in het team zet dan krijgt hij hiervan een melding en wordt de pokemon niet toegevoegd
    * Als de pokemon niet bekend is (dus het ingegeven nummer groter is dan de lengte van de array) wordt er ook een melding gegeven
    * Als je STOP hebt ingegeven dan wordt het team van de gebruiker op het scherm getoond
@@ -26,9 +28,10 @@
    3  vraag invoer om team aan te vullen
    4  team aanvullen
      uitzonderingen
-     a  pokemon al in het team
-     b  pokemon niet bekend
-     c  gebruik voert STOP in
+     a  pokemon al in het team (Deze pokemon zit al in je team)
+     b  pokemon niet bekend (Deze pokemon ken ik niet)
+     c  gebruiker voert STOP in (toon team)
+        --Jouw team van pokemon is:
 
 */
 
