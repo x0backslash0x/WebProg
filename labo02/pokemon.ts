0ddc96da16387521673ecfc2fee7d4bfd3399b46
x0backslash0x <paddy0524@gmail.com>
2024-10-02 18:46:14 +0200

    maak bestand aan voor oefening 6

---
+++ labo02/pokemon.ts
@@ -0,0 +1,12 @@
+/* pokemon.ts
+
+   2024-2025 Patrick Lanove
+   BA Electronica-ICT/Cybersecurity
+   OLOD Web programming
+
+   labo 2 oefening 6
+
+   ### OPGAVE ###
+   Gegeven is de volgende array van 20 pokemon:
+
+*/
