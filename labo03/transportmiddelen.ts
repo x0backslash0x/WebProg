859d7f0ba818a430a9ace236287967eeaf659b1a
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-03 15:29:14 +0200

    oef 2 zet bestand klaar

---
+++ labo03/transportmiddelen.ts
@@ -0,0 +1,15 @@
+/* transportmiddelen.ts
+
+   2024-2025 Patrick Lanove
+   BA Electronica-ICT/Cybersecurity
+   OLOD Web programming
+
+   labo 3 oefening 2
+
+   ### WERKWIJZE ###
+   1  interfaces
+   2  classes - 1 testinstantie elks
+   3  rijden() method
+   4  bijkomende instanties
+*/
+
