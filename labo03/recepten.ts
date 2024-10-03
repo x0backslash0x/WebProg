6a310deaa2a0129b9bace76d965452f8fa04b474
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-03 10:21:58 +0200

    oef 0301 interfaces aanmaken

--- labo03/recepten.ts
+++ labo03/recepten.ts
@@ -19,28 +19,3 @@ interface Recepten {
     personen: number;
     ingredienten: Ingredient[];
 }
-
-let ingredient1: Ingredient = {
-    naam: 'lasagnevellen',
-    hoeveelheid: '1 pak',
-    prijs: 3
-}
-
-let ingredient2: Ingredient = {
-    naam: 'gehakt',
-    hoeveelheid: '500 gr',
-    prijs: 4
-}
-
-let ingredient3: Ingredient = {
-    naam: 'look',
-    hoeveelheid: '1 teentje',
-    prijs: 3
-}
-
-let recept: Recepten = {
-    naam: 'Lasagne',
-    beschrijving: 'Lekker lasagne',
-    personen: 4,
-    ingredienten: [ingredient1, ingredient2, ingredient3]
-}
