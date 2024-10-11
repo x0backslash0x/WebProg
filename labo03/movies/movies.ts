4005bb45231e343eeebf111f658ff44cea426194
x0backslash0x <paddy0524@gmail.com>
2024-10-11 14:38:46 +0200

    labo 3, oefening 3 bestand klaar zetten

---
+++ labo03/movies/movies.ts
@@ -0,0 +1,19 @@
+/* movies.ts
+
+   2024-2025 Patrick Lanove
+   BA Electronica-ICT/Cybersecurity
+   OLOD Web programming
+
+   labo 3 oefening 3
+
+    WERKWIJZE
+  x1 Movie class
+  x2 instance myFavoriteMovie
+  x3 print method voor class
+  x4 instance myWorstMovie
+  x5 instance myLastMovie
+  x6 functie wasMovieMadeInThe90s
+  x7 functie averageMetaScore
+  x8 functie fakeMetaScore
+
+*/
