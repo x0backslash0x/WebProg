c2b3712f56dca2a096761158fd23ca72d2668b51
x0backslash0x <paddy0524@gmail.com>
2024-10-11 14:33:44 +0200

    labo 3, oefening 2 bestand klaar zetten

---
+++ labo03/transportmiddelen/transportmiddelen.ts
@@ -0,0 +1,14 @@
+/* transportmiddelen.ts
+
+   2024-2025 Patrick Lanove
+   BA Electronica-ICT/Cybersecurity
+   OLOD Web programming
+
+   labo 3 oefening 2
+
+   ### WERKWIJZE ###
+   1x interface
+   2x classes - 1 testinstantie elks
+   3x rijden() method
+   4x bijkomende instanties
+*/
