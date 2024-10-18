f1130c5b96fad801a3d5bcfc1eef02c61cb2f0c6
x0backslash0x <paddy0524@gmail.com>
2024-10-18 15:19:04 +0200

    bestand aanmaken

---
+++ labo-04/coctail-promise-all/coctail-promise-all.ts
@@ -0,0 +1,10 @@
+//coctail-promise-all
+//
+// 2024-2025 Patrick Lanove
+// BA Electronica-ICT/Cybersecurity
+// OLOD Web programming
+//
+// labo 4, oefening 2
+//
+// URL van de coctail API
+// https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000
