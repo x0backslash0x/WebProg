6cf9c84b301eb1c87aae98e81c8b1fd5891ae5d8
x0backslash0x <paddy0524@gmail.com>
2024-10-21 11:09:29 +0200

    labo 4, oefening 2: opgave toevoegen

--- labo-04/coctail-promise-all/coctail-promise-all.ts
+++ labo-04/coctail-promise-all/coctail-promise-all.ts
@@ -6,5 +6,11 @@
 //
 // labo 4, oefening 2
 //
+// ### OPGAVE ###
+//  Maak gebruik van Promise.all om de drie volgende cocktails via de cocktail api met de volgende ids in te lezen:
+//  +11000, 11001, 11002
+//
+/// vervolgens de naam van de drie cocktails op het scherm te laten zien.
+//
 // URL van de coctail API
 // https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000
