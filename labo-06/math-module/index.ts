a2e1e913bad24b237ed458b725d7081512066620
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-24 10:19:48 +0200

    labo 6 oefening 1 klaar

---
+++ labo-06/math-module/index.ts
@@ -0,0 +1,15 @@
+//index.ts
+//
+// 2024-2025 Patrick Lanove
+// BA Electronica-ICT/Cybersecurity
+// OLOD Web programming
+//
+// labo 6 oefening 1
+// de code in dit bestand voert de module math-module uit
+
+import {add, substract, multiply, divide} from "./math";
+
+console.log(add(2, 19));
+console.log(substract(23,11));
+console.log(multiply(4,9));
+console.log(divide(6,2));
