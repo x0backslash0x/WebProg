ddd772bf2560db13f9715ad2d0af29fd9ade39ca
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-25 11:41:02 +0200

    labo 5 oefening 1 klaar

---
+++ labo-05/math-module/math.ts
@@ -0,0 +1,24 @@
+//math.ts
+//
+// 2024-2025 Patrick Lanove
+// BA Electronica-ICT/Cybersecurity
+// OLOD Web programming
+//
+// labo 6 oefening 1
+// dit is de module math-module
+
+export function add(a: number, b: number): number {
+    return a + b;
+}
+
+export function substract(a: number, b: number): number {
+    return a - b;
+}
+
+export function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+export function divide(a: number, b: number): number {
+    return a / b;
+}
