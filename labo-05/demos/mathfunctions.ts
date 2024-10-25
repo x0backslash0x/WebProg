37ba42d86db57e44a40dcb370a03a25b381adce0
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-25 11:31:21 +0200

    labo 5 demos toevoegen

---
+++ labo-05/demos/mathfunctions.ts
@@ -0,0 +1,7 @@
+export function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+export function divide(num1: number, num2: number): number {
+    return num1 / num2;
+}
