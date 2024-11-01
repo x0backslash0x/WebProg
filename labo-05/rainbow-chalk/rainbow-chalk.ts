94b098aeca14ac271769de8bb04492752a688ce7
x0backslash0x <paddy0524@gmail.com>
2024-11-01 17:27:32 +0100

    labo 5, oefening 4 klaar

--- labo-05/rainbow-chalk/rainbow-chalk.ts
+++ labo-05/rainbow-chalk/rainbow-chalk.ts
@@ -5,3 +5,21 @@
 // OLOD Web programming
 //
 // labo 5 oefening 4 rainbow chalk
+
+import { RgbColor } from "rainbow-colors-array-ts";
+import { rainbow } from "rainbow-colors-array-ts";
+import chalk from "chalk";
+
+const height = process.stdout.rows;
+const width = process.stdout.columns;
+
+const colorsArray = rainbow(height, "rgb", true);
+
+let charlen: string = ' '
+for (let i: number = 1; i < width; i++) {
+    charlen = charlen + ' '
+}
+
+for (var color of colorsArray) {
+    console.log(chalk.bgRgb(color['r'], color['g'], color['b']).bold(charlen));
+}
