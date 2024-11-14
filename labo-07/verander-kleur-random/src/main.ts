c458fa738e71abd9099933efc56406d2c221effe
x0backslash0x <paddy0524@gmail.com>
2024-11-14 21:15:07 +0100

    labo 7 oefening 5 genereren tabel naar functie verplaatsen

--- labo-07/verander-kleur-random/src/main.ts
+++ labo-07/verander-kleur-random/src/main.ts
@@ -6,7 +6,7 @@ const button2 = document.querySelector("button");
 const button4 = document.querySelector("#knop4");
 const body = document.querySelector("body");
 
-button2?.addEventListener("click", () => {
+function createTable(colls: number): HTMLTableElement | undefined {
   const table = document.querySelector("table");
 
   if(table) {
@@ -16,11 +16,10 @@ button2?.addEventListener("click", () => {
   const newtable = document.createElement("table");
   const tbody = document.createElement("tbody");
   newtable.appendChild(tbody);
-  body?.appendChild(newtable);
 
   for(let i = 1; i < 5; i++) {
     const row = document.createElement("tr");
-    for(let i = 1; i < 3; i++) {
+    for(let i = 1; i < colls + 1; i++) {
       const cell = document.createElement("td");
       const rand = Math.floor((Math.random() * 100));
       cell.appendChild(document.createTextNode(String(rand)));
@@ -29,7 +28,14 @@ button2?.addEventListener("click", () => {
 
     tbody.appendChild(row);
   }
-  
+
+  return newtable;
+}
+
+button2?.addEventListener("click", () => {
+  const table = createTable(2);
+  body?.appendChild(table);
+
   const cells = document.querySelectorAll("td");
   for(let i = 0; i < cells.length; i++) {
     const cell = cells[i];
@@ -40,28 +46,8 @@ button2?.addEventListener("click", () => {
 })
 
 button4?.addEventListener("click", () => {
-  const table = document.querySelector("table");
-
-  if(table) {
-    body?.removeChild(table);
-  }
-
-  const newtable = document.createElement("table");
-  const tbody = document.createElement("tbody");
-  newtable.appendChild(tbody);
-  body?.appendChild(newtable);
-
-  for(let i = 1; i < 5; i++) {
-    const row = document.createElement("tr");
-    for(let i = 1; i < 5; i++) {
-      const cell = document.createElement("td");
-      const rand = Math.floor((Math.random() * 100));
-      cell.appendChild(document.createTextNode(String(rand)));
-      row.appendChild(cell);
-    }
-
-    tbody.appendChild(row);
-  }
+  const table = createTable(4);
+  body?.appendChild(table);
 
   const cells = document.querySelectorAll("td");
   for(let i = 0; i < cells.length; i++) {
