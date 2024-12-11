e6c04a0d66c85446f839312484a34b107275e85f
x0backslash0x <paddy0524@gmail.com>
2024-12-11 13:14:14 +0100

    0903 classes aanmaken

--- labo-09/reiskosten/server.ts
+++ labo-09/reiskosten/server.ts
@@ -5,3 +5,44 @@
 // OLOD Web programming
 //
 // labo 9 oefening 3 reiskosten server
+
+import express, {Express, Request, Response} from "express";
+
+class Reis {
+    #id: number;
+    #bestemming: string;
+    #jaar: number;
+    #kosten: Kost[];
+
+    constructor(id: number, bestemming: string, jaar: number) {
+        this.#id = id;
+        this.#bestemming = bestemming;
+        this.#jaar = jaar;
+    }
+
+    toJSON() {
+        return {
+            "id": this.#id,
+            "bestemming": this.#bestemming,
+            "jaar": this.#jaar,
+            "kosten": this.#kosten
+        }
+    }
+}
+
+class Kost {
+    #uitgave: string;
+    #prijs: number;
+
+    constructor(uitgave: string, prijs: number)  {
+        this.#uitgave = uitgave;
+        this.#prijs = prijs;
+    }
+
+    toJSON() {
+        return {
+            "uitgave": this.#uitgave,
+            "prijs": this.#prijs
+        }
+    }
+}
