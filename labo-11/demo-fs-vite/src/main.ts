ca382ea90d9a81a9a70551e7064c08e899724d5e
Patrick Lanove <patrick.lanove@student.ap.be>
2024-12-12 09:53:55 +0100

    1100 demo frontend afhandeling toevoegen

--- labo-11/demo-fs-vite/src/main.ts
+++ labo-11/demo-fs-vite/src/main.ts
@@ -1,1 +1,43 @@
 import './style.css'
+
+const formElement = document.querySelector("#myForm");
+const formMessage = document.querySelector("#resoneMessage");
+
+interface MyFormData {
+  name: string;
+  email: string;
+}
+
+formElement?.addEventListener("submit", async (event) => { // async kan ook gebruikt worden bij arrow functions
+  event.preventDefault // default gedrag van submit tegenhouden en controle overnemen
+
+/*   const form = event.target;
+  const formData: = new MyFormData(form);
+  const data: MyFormData = Object.fromEntries(formData.entries()); */
+
+  const nameFormElement = document.querySelector("#name");
+  const emailormElement = document.querySelector("#email");
+  let data: MyFormData = {
+    name = nameFormElement?.nodeValue;
+    email = emailormElement?.nodeValue;
+  }
+
+  try {
+    const response = await fetch("http://localhost:3000/sendData", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+
+    const message = await response.text();
+    formMessage?.textContent = message;
+    });
+  } catch (error) {
+    console.log(error);
+    formMessage?.textContent = "Er is een fout opgetreden";
+  }
+
+
+
+});
