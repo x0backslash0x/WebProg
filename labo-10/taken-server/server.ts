f73363454d3d432b5a081186c702dfbf51774153
Patrick Lanove <patrick.lanove@student.ap.be>
2024-12-12 16:05:13 +0100

    1002 klasse taak aanmaken

--- labo-10/taken-server/server.ts
+++ labo-10/taken-server/server.ts
@@ -6,3 +6,13 @@
  * 
  * labo 10 oefening 2 taken server
  */
+
+class Taak {
+    omschrijving: string;
+    naam: string;
+
+    constructor(omschrijving: string, naam: string) {
+        this.omschrijving = omschrijving;
+        this.naam = naam;
+    }
+}
