b0441dbcbbcacb79ae6b2ffbd4d8944542758c47
Patrick Lanove <patrick.lanove@student.ap.be>
2024-12-12 09:52:59 +0100

    1100 demo backend afhandeling toevoegen

---
+++ labo-11/demo-fs-server/server.ts
@@ -0,0 +1,39 @@
+import express, {Express, Request, Response} from 'express';
+import mysql, { ConnectionOptions, Connection } from 'mysql2';
+
+const app: Express = express();
+app.use(express.json());
+
+const hostname: string = "127.0.0.1";
+const port: number = 3000;
+
+const access: ConnectionOptions = {
+    host: "localhost",
+    user: "root",
+    password: "hierjourwachtwoord",
+    database: "demo"
+}
+
+interface MyFormData {
+    name: string;
+    email: string;
+}
+
+app.post("/sendData", async (req: Request, res: Response) => {
+    let json: MyFormData = req.body;
+    console.log(json);
+
+    try {
+        const conn: Connection = await mysql.createConnection(access);
+        const [result] = await conn.query("INSERT INTO USERS(name, email) VALUES(?, ?)"),
+            ([json.name, json.email]);    
+        res.status(200).send("Data is aangekomen");
+    } catch (error) {
+        res.status(500).send("Er is een fout opgetreden");
+    }
+
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server listening on http://${hostname}:${port}`);
+});
